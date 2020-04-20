09e387eb323d650f17ba3c6d539634b5af68f654
ishaparveen <nidhi.gupta_cs18@gla.ac.in>
2020-04-20 16:49:02 +0530

    occurence fn complted

--- src/occurence/MyOccurence.java
+++ src/occurence/MyOccurence.java
@@ -1,9 +1,18 @@
 package occurence;
 
 public class MyOccurence {
-    public static int occurence(int arr[], int element) {
+    public static boolean occurence(int arr[], int element) {
+        int counter = 0;
         for (int i = 0; i < arr.length; i++) {
-
+            if (arr[i] == element) {
+                System.out.println(i + 1);
+                ++counter;
+            }
         }
+        if (counter > 1)
+            return true;
+        else
+            return false;
     }
+
 }
