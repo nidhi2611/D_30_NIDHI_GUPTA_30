a759530ef33b6b87b4e2f0864d54c6b07ead30f7
ishaparveen <nidhi.gupta_cs18@gla.ac.in>
2020-04-20 17:52:50 +0530

    elements entered into the ARRAY

--- src/duplicate/CalculateDuplicate.java
+++ src/duplicate/CalculateDuplicate.java
@@ -5,5 +5,14 @@ import java.util.Scanner;
 public class CalculateDuplicate {
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the size of the array");
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+        System.out.println("enter the elements in the array");
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+
     }
 }
