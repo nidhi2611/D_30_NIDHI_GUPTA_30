5c61beccf991a310efa284442bc28bff6ebad2e5
ishaparveen <nidhi.gupta_cs18@gla.ac.in>
2020-04-20 17:38:07 +0530

    program compiled

--- src/occurence/MyOccurence.java
+++ src/occurence/MyOccurence.java
@@ -7,7 +7,7 @@ public class MyOccurence {
         int counter = 0;
         for (int i = 0; i < arr.length; i++) {
             if (arr[i] == element) {
-                System.out.println("elment found at index+"i + 1);
+                System.out.println("elment found at index+" + i + 1);
                 ++counter;
             }
         }
@@ -18,7 +18,7 @@ public class MyOccurence {
         System.out.println("enter the size of the arrray");
         int n = sc.nextInt();
         int arr[] = new int[n];
-        System.out.println("Enter the elements in the array")
+        System.out.println("Enter the elements in the array");
         for (int j = 0; j < arr.length; j++) {
             arr[j] = sc.nextInt();
         }
