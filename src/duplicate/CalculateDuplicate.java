36ba7e72e028a4d6683bf517196e279d13f54050
ishaparveen <nidhi.gupta_cs18@gla.ac.in>
2020-04-20 17:48:24 +0530

    Scanner class imported

--- src/duplicate/CalculateDuplicate.java
+++ src/duplicate/CalculateDuplicate.java
@@ -1,7 +1,9 @@
 package duplicate;
 
+import java.util.Scanner;
+
 public class CalculateDuplicate {
     public static void main(String args[]) {
-
+        Scanner sc = new Scanner(System.in);
     }
 }
