aa6ee08ea4355072e4115d0723dfcd8317488e59
ishaparveen <nidhi.gupta_cs18@gla.ac.in>
2020-04-20 17:35:48 +0530

    function occurence called

--- src/occurence/MyOccurence.java
+++ src/occurence/MyOccurence.java
@@ -3,7 +3,7 @@ package occurence;
 import java.util.Scanner;
 
 public class MyOccurence {
-    public static boolean occurence(int arr[], int element) {
+    public static void occurence(int arr[], int element) {
         int counter = 0;
         for (int i = 0; i < arr.length; i++) {
             if (arr[i] == element) {
@@ -11,14 +11,18 @@ public class MyOccurence {
                 ++counter;
             }
         }
-        if (counter > 1)
-            return true;
-        else
-            return false;
     }
 
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
-
+        System.out.println("enter the size of the arrray");
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+        for (int j = 0; j < arr.length; j++) {
+            arr[j] = sc.nextInt();
+        }
+        System.out.println("enter the element");
+        int element = sc.nextInt();
+        occurence(arr, element);
     }
 }
